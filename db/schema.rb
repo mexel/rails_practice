b08383415e7e4f41206c1eaa58359780d56da9fe
sugiyama <sugiyamagenna@gan-hi-noiMac.local>
2014-02-17 13:34:47 +0900

    ユーザーに年齢カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140210102224) do
+ActiveRecord::Schema.define(version: 20140217042625) do
 
   create_table "hoges", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140210102224) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "age"
   end
 
   create_table "weapons", force: true do |t|
